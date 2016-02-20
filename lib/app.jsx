338b31c4cab1f6c555debefe82c4226008a2a708
Derek Ekins <derek@spathi.com>
2016-02-20 10:07:10 +0000

    implemented show top stories

--- lib/app.jsx
+++ lib/app.jsx
@@ -5,11 +5,22 @@ var httpism = require('httpism');
 
 module.exports = class App {
   constructor(config) {
+    this.topStories = [];
+    httpism.get('/top-stories').then((response) => {
+      this.topStories = response.body;
+      this.refresh();
+    });
+  }
+
+  renderStory(story){
+    return <div class="story">{story.title}</div>;
   }
 
   render(){
+    this.refresh = h.refresh;
     return <div>
       <h1>Monkey News</h1>
+      {this.topStories.map(this.renderStory)}
     </div>;
   }
 }
