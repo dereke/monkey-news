638a34cebf41064b870298e8b4a6dd5d0d44e237
Derek Ekins <derek@spathi.com>
2016-02-20 09:59:35 +0000

    added expected stories to fake api

--- test/topStoriesSpec.js
+++ test/topStoriesSpec.js
@@ -4,6 +4,13 @@ var createApi = require('./createApi');
 
 describe('top stories', () => {
   it('shows the top x stories', () => {
+    var api = createApi();
+    api.stories = [
+      { title: 'Monkey escapes from zoo' },
+      { title: 'Research shows monkeys make better leaders' },
+      { title: 'In time monkeys will probably have their own planet' }
+    ];
+
     mount();
 
     return browser.find('.story').shouldHave({
