390157948649949fb2cdbe296ca1a7ab7d0eebcb
Derek Ekins <derek@spathi.com>
2016-02-20 11:36:16 +0000

    added test for creating a story

---
+++ test/createStorySpec.js
@@ -0,0 +1,18 @@
+var browser = require('browser-monkey');
+var mount = require('./mount');
+var createApi = require('./createApi');
+
+describe('create story', () => {
+  it('adds a story to top stories', () => {
+    mount();
+
+    return browser.find('input.story-title').typeIn('Monkey goes crazy').then(() => {
+      return browser.find('button.story-submit').click();
+    }).then(() => {
+      return Promise.all([
+        browser.find('.story').shouldHave({text: 'Monkey goes crazy'}),
+        browser.find('input.story-title').shouldHave({exactValue: ''})
+      ]);
+    });
+  });
+});
