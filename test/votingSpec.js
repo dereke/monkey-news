25fb63c282262b4e51903b4b84d4b2c0bea47dba
Derek Ekins <derek@spathi.com>
2016-02-20 11:23:44 +0000

    added test for vote down - extracted page model

--- test/votingSpec.js
+++ test/votingSpec.js
@@ -2,6 +2,12 @@ var browser = require('browser-monkey');
 var mount = require('./mount');
 var createApi = require('./createApi');
 
+var story = browser.find('.story', {text: 'Monkey escapes from zoo'}).component({
+  votes: function() { return this.find('.votes'); },
+  voteUp: function() { return this.find('.vote-up'); },
+  voteDown: function() { return this.find('.vote-down'); }
+});
+
 describe('voting', () => {
   it('can vote up a story', () => {
     var api = createApi();
@@ -12,10 +18,26 @@ describe('voting', () => {
 
     mount();
 
-    return browser.find('.story', {text: 'Monkey escapes from zoo'}).find('.votes').shouldHave({text: '0'}).then(() => {
-      return browser.find('.story', {text: 'Monkey escapes from zoo'}).find('.vote-up').click();
+    return story.votes().shouldHave({text: '0'}).then(() => {
+      return story.voteUp().click();
     }).then(() => {
-      return browser.find('.story', {text: 'Monkey escapes from zoo'}).find('.votes').shouldHave({text: '1'});
+      return story.votes().shouldHave({text: '1'});
+    });
+  });
+
+  it('can vote down a story', () => {
+    var api = createApi();
+    api.stories = [
+      { title: 'Monkey escapes from zoo', votes: 3 },
+      { title: 'Research shows monkeys make better leaders', votes: 0 }
+    ];
+
+    mount();
+
+    return story.votes().shouldHave({text: '3'}).then(() => {
+      return story.voteDown().click();
+    }).then(() => {
+      return story.votes().shouldHave({text: '2'});
     });
   });
 });
