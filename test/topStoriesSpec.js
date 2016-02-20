e4388a85f47441ebc2d73481ab41261498c23c89
Derek Ekins <derek@spathi.com>
2016-02-20 09:56:44 +0000

    failing tests for top stories

--- test/topStoriesSpec.js
+++ test/topStoriesSpec.js
@@ -3,5 +3,15 @@ var mount = require('./mount');
 var createApi = require('./createApi');
 
 describe('top stories', () => {
-  it('shows the top x stories');
+  it('shows the top x stories', () => {
+    mount();
+
+    return browser.find('.story').shouldHave({
+      length: 3, text: [
+        'Monkey escapes from zoo',
+        'Research shows monkeys make better leaders',
+        'In time monkeys will probably have their own planet'
+      ]
+    });
+  });
 });
